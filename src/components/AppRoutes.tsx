4a4e5117a2005bc27f5492cde0f8f62c961e5b42
solo5star <solo_5@naver.com>
2022-06-02 07:43:16 +0900

    fix: fix omitted routes and imports in `AppRoutes`

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -4,6 +4,13 @@ import { Route, Routes } from 'react-router';
 import { MtlBaseLayout } from './layouts/MtlBaseLayout';
 import { Index } from './pages/Index';
 import { Login, LoginCallback } from './pages/Login';
+import { MinecraftMap } from './pages/MinecraftMap';
+import { NavBarEdit } from './pages/NavBarEdit';
+import { NoticeAdd } from './pages/NoticeAdd';
+import { Notices } from './pages/Notices';
+import { Notion } from './pages/Notion';
+import { Profile } from './pages/Profile';
+import { Users } from './pages/Users';
 
 export { AppRoutes };
 
@@ -18,6 +25,16 @@ const AppRoutes: React.FC = () => {
 						<Routes>
 							<Route index element={<Login />} />
 							<Route path="callback" element={<LoginCallback />} />
+						</Routes>
+					}
+				/>
+				<Route
+					path="notices/*"
+					element={
+						<Routes>
+							<Route index element={<Notices />} />
+							<Route path=":pageId" element={<Notion />} />
+							<Route path="add" element={<NoticeAdd />} />
 						</Routes>
 					}
 				/>
