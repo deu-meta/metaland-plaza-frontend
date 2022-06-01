b60db73a9ee4ff94505683faba4a17327777757b
solo5star <solo_5@naver.com>
2022-06-02 07:18:02 +0900

    feat: add recoil root to use recoil atoms

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import { App } from './components/App';
 import { UserProvider } from './context/user';
@@ -9,13 +10,15 @@ import { queryClient } from './services/api';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<UserProvider>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</UserProvider>
-		</QueryClientProvider>
+		<RecoilRoot>
+			<QueryClientProvider client={queryClient}>
+				<UserProvider>
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+				</UserProvider>
+			</QueryClientProvider>
+		</RecoilRoot>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
