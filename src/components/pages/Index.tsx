959fd379689f3c4b96747f20e60b9010e65e6d4f
solo5star <solo_5@naver.com>
2022-06-08 22:27:41 +0900

    feat: implement index page redirection (to home navlink)

--- src/components/pages/Index.tsx
+++ src/components/pages/Index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+
+import { useNavlinks } from '../../actions/navbar';
+import { MtlPageContents } from '../basics/MtlPageContents';
+import { MtlPageTitle } from '../basics/MtlPageTitle';
 
 export { Index };
 
 const Index: React.FC = () => {
-	return <>Index Page</>;
+	const navigate = useNavigate();
+	const { homeNavlink } = useNavlinks();
+	if (homeNavlink.link !== '/') {
+		navigate(homeNavlink.link, { replace: true });
+	}
+
+	return (
+		<MtlPageContents>
+			<MtlPageTitle>비어있는 페이지입니다.</MtlPageTitle>
+		</MtlPageContents>
+	);
 };
